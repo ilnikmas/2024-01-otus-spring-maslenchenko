cc571b70bc564f4c9633f2f97b7cbeabfb8372f2
Ilya Maslenchenko <ilnikmas@tut.by>
2024-02-25 11:07:41 +0300

    #hw03 initioal commit

--- hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,12 +1,19 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -15,11 +22,15 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        Resource resource = new ClassPathResource(fileNameProvider.getTestFileName());
+        List<QuestionDto> questions;
+        try {
+            questions = new CsvToBeanBuilder<QuestionDto>(new InputStreamReader(resource.getInputStream()))
+                    .withSeparator(';')
+                    .withType(QuestionDto.class).build().parse();
+        } catch (IOException e) {
+            throw new QuestionReadException("Error while reading file", e);
+        }
+        return questions.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
     }
 }
--- hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,14 +3,16 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
-@Service
 @RequiredArgsConstructor
+@Service
 public class TestServiceImpl implements TestService {
 
-    private final LocalizedIOService ioService;
+    private final IOService ioService;
 
     private final QuestionDao questionDao;
 
@@ -25,9 +27,25 @@ public class TestServiceImpl implements TestService {
 
         for (var question: questions) {
             var isAnswerValid = false; // Задать вопрос, получить ответ
+            int max = question.answers().size();
+            int answerIndex = ioService.readIntForRangeWithPrompt(
+                    1,
+                    max,
+                    questionToString(question),
+                    "Введите целое число от 1 до " + max) - 1;
+            isAnswerValid = question.answers().get(answerIndex).isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
     }
 
+    private String questionToString(Question question) {
+        StringBuilder resultString = new StringBuilder(question.text()).append("\n");
+        for (int i = 0; i < question.answers().size(); i++) {
+            resultString.append(i + 1).append(". ");
+            resultString.append(question.answers().get(i).text());
+            resultString.append("\n");
+        }
+        return resultString.toString();
+    }
 }
